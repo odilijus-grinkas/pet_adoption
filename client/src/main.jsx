0a92b1ca75a04e6f9acb647fc5a803aa6c4ce1bc
odilijus-grinkas <odilijus.grinkas@stud.ktmc.lt>
2024-02-13 10:49:53 +0200

    React Next changed to react Vite

---
+++ client/src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import Index from "./primary_comps/index";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Index />
+  </React.StrictMode>
+);
