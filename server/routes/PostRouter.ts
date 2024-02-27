c29520ac4648bd40a893531b75d069784d028595
DominykasKupsys <dominykas.kupsys@stud.ktmc.lt>
2024-02-27 10:57:33 +0200

    parasiau dokumentacija, sutaisiau routeri

--- server/routes/PostRouter.ts
+++ server/routes/PostRouter.ts
@@ -7,7 +7,7 @@ const postsRouter = Router()
 postsRouter.get("/post/all", getAllPosts)
 postsRouter.get("/post/:id", getOnePost)
 postsRouter.post("/post/create", createPost)
-postsRouter.put("post/:id", updatePost)
-postsRouter.delete("post/:id", deletePost)
+postsRouter.put("/post/:id", updatePost)
+postsRouter.delete("/post/:id", deletePost)
 
 export default postsRouter
